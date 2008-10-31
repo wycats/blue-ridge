9aa9958eb6942edac2a6b1be7b3673474999141c
Lawrence Karnowski <larry@thinkrelevance.com>
2008-10-31 14:58:11 -0400

    added the js-spec.css file

--- generators/javascript_testing/javascript_testing_generator.rb
+++ generators/javascript_testing/javascript_testing_generator.rb
@@ -3,10 +3,11 @@ class JavascriptTestingGenerator < Rails::Generator::Base
     record do |m|
       m.directory 'test/javascript'
       m.file 'application_spec.js', 'test/javascript/application_spec.js'
-      m.file 'spec_helper.js', 'test/javascript/spec_helper.js'
+      m.file 'spec_helper.js',      'test/javascript/spec_helper.js'
       
       m.directory 'test/javascript/fixtures'
       m.file 'application.html', 'test/javascript/fixtures/application.html'
+      m.file 'js-spec.css',      'test/javascript/fixtures/js-spec.css'
     end
   end
 end
