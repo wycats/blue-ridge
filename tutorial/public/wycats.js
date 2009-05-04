3d78eeec25c62c07a964b578e4cbc3ff0d92bbbc
Yehuda Katz <wycats@gmail.com>
2009-05-04 04:58:05 -0700

    Some documentation for setupPlugin.

--- tutorial/public/wycats.js
+++ tutorial/public/wycats.js
@@ -1,3 +1,59 @@
+// Run a set of functions passed in by the user against
+// the jQuery object that is being set up in the plugin
+// and the options that were passed in by the user for
+// the plugin.
+//
+// Example:
+//
+//   $.fn.simplePlugin = function(options) {
+//     this.setupPlugin(options.setup);
+//   
+//     this.click(function() {
+//       $(this).trigger("activated", $(this).html());
+//     })
+//   }
+//   
+//   var logger = {
+//     click: function(options) {
+//       logText = options.text;
+//       this.bind("activated", function(e, html) { 
+//         console.log(text + ": " + html); 
+//       })
+//     } 
+//   }
+//   
+//   $("div").simplePlugin({setup: logger, text: "LOG"});
+//
+// When the plugin is activated, the functions in logger are called,
+// passing in the user options. In this case, all divs on the page
+// will output "LOG: #{their innerHTML}" when clicked on.
+//
+// The purpose of this plugin is to make it easier to separate the core
+// functionality of a plugin from its auxiliary parts. In particular, it
+// makes it easy to split out the basic, low-level event handling from 
+// the high-level event handling, and makes it easier to provide alternative 
+// or modified implementations of the default high-level event handlers
+// that are provided with a plugin.
+//
+// It is expected that plugin authors provide a default setup
+// implementation, preferably stored in $.fn.pluginName.base or something
+// like that.
+//
+// Note that in addition to supporting single functions bound to specific
+// names, setupPlugin also supports arrays of functions. This makes it
+// easier for modifications to be made to the default setup that require
+// inserting a function *before* the one provided by default, but still
+// maintaining the default behavior as well. As a result, the preferred
+// usage is:
+// 
+//   var logger = {
+//     click: [function(options) {
+//       logText = options.text;
+//       this.bind("activated", function(e, html) { 
+//         console.log(text + ": " + html); 
+//       })
+//     }]
+//   }
 jQuery.fn.setupPlugin = function(setup, options) {
   for(extra in setup) {
     var self = this;
